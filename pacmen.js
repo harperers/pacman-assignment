a98ad34defef6b3cc2cb40827ffe0adf9e8f2c16
harperers <max@pan-ami.com>
2024-03-21 19:49:50 -0700

    INITIAL COMMIT

--- pacmen.js
+++ pacmen.js
@@ -12,22 +12,15 @@ function setToRandom(scale) {
 function makePac() {
   // returns an object with random values scaled {x: 33, y: 21}
   let velocity = setToRandom(10); // {x:?, y:?}
-  let position = setToRandom(200);
-
-  // Add image to div id = game
-  let game = document.getElementById('game');
-  let newimg = document.createElement('img');
-  newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
-  newimg.width = 100;
-
-  // TODO: set position here
-
-
-  // TODO add new Child image to game
-  game.appendChild(/* TODO: add parameter */);
-
-  // return details in an object
+  let position = setToRandom(200); // Add image to div id = game
+  let game = document.getElementById("game");
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
+  newimg.src = "./images/PacMan1.png";
+  newimg.width = 100; // TODO: set position here*
+  newimg.style.left = position.x + "px";
+  newimg.style.top = position.y + "px"; // TODO add new Child image to game
+  game.appendChild(newimg); // return details in an object
   return {
     position,
     velocity,
@@ -50,7 +43,19 @@ function update() {
 
 function checkCollisions(item) {
   // TODO: detect collision with all walls and make pacman bounce
-  
+
+
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  )
+    item.velocity.x = -item.velocity.x;
+  if (
+    item.position.y + item.velocity.y + item.newimg.height >
+      window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  )
+    item.velocity.y = -item.velocity.y;
 }
 
 function makeOne() {
